dc7c61ecdfc91113391405fc681f6dc3e1f67ab8
lugrim <luisgrimaldi20@gmail.com>
2025-09-16 18:54:11 -0400

    v4

--- api/index.ts
+++ api/index.ts
@@ -4,8 +4,9 @@ import serverless from "serverless-http";
 const app = express();
 
 // Ejemplo API
-app.get("/api/hello", (req, res) => {
+app.get("/hello", (req, res) => {
   res.json({ message: "Hola desde Express en Vercel 🚀" });
 });
 
 export default serverless(app);
+
